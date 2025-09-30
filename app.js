fa67d07eb13392df43df754ab79ccc721dddf629
Agustin Adolfo Barrio Eastman <agustinadolfobarrioeastman@Agustin.local>
2025-09-29 23:04:27 -0300

    some customizations for project

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -14,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -29,8 +27,14 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  const status = err.status || 500;
+  res.status(status);
+  res.json({
+    error: {
+      message: err.message,
+      code: status
+    }
+  });
 });
 
 module.exports = app;
